9d1afa74a74e4d40cca360616d90b268352a482d
KARIML <KARIML@PC2016-10>
2019-04-26 15:54:55 +0200

    Adding a One-to-Many Entity Relationship5:48 with James Churchill

--- ComicBookGalleryModel/Models/ComicBook.cs
+++ ComicBookGalleryModel/Models/ComicBook.cs
@@ -8,11 +8,14 @@ namespace ComicBookGalleryModel.Models
 {
     public class ComicBook
     {
+       // EF only adds table columns for entity properties that have a setter—public, protected, or private.
+
         // ID wordt automatisch PrimaryKey in SQL en omdat het een numeriek veld is wordt dit automatisch ook Identity
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string Description { get; set; }
         public DateTime PublishedOn { get; set; }
+        public decimal? Averagerating { get; set; } //questionMark to make it nullable
     }
 }
--- ComicBookGalleryModel/Program.cs
+++ ComicBookGalleryModel/Program.cs
@@ -17,16 +17,18 @@ namespace ComicBookGalleryModel
             {
                 context.ComicBooks.Add(new ComicBook()
                 {
-                    SeriesTitle = "The Amazing Spiderman",
+                    SeriesTitle = "The Amazing SpiderBoy",
                     IssueNumber = 1,
-                    PublishedOn = DateTime.Today
+                    PublishedOn = DateTime.Today,
+                    Description= "An amazing movie"
+ 
                 });
                 context.SaveChanges();
                 // to persist the added entity to the DB
                 var comicbooks = context.ComicBooks.ToList(); // this will retrieve all the comic books from the database
                 foreach (var item in comicbooks)
                 {
-                    Console.WriteLine(item.SeriesTitle);
+                    Console.WriteLine(item.SeriesTitle +" "+ item.Description);
                 }
                 Console.ReadLine();
             } 
